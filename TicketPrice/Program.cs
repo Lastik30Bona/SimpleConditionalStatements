e9cf1c5e42f775a08fac3ec8658372908acdbaad
GRIGS <GRIGS@DESKTOP-LM1QH8N>
2024-09-18 22:04:43 +0300

    Add project files.

---
+++ TicketPrice/Program.cs
@@ -0,0 +1,14 @@
+﻿string price = Console.ReadLine();
+
+if (price == "student")
+{
+    Console.WriteLine("$1.00");
+}
+else if (price == "regular")
+{
+    Console.WriteLine("$1.60");
+}
+else
+{
+    Console.WriteLine("Invalid ticket type!");
+}
---
+++ ValidTriangle/Program.cs
@@ -0,0 +1,22 @@
+﻿int firstSide = int.Parse(Console.ReadLine());
+int secondSide = int.Parse(Console.ReadLine());
+int thirdSide = int.Parse(Console.ReadLine());
+
+//i need to add one more thing to work, mby tomorrow
+
+if (firstSide == secondSide + thirdSide)
+{
+    Console.WriteLine("Valid Triangle");
+}
+else if (secondSide == firstSide + thirdSide)
+{
+    Console.WriteLine("Valid Triangle");
+}
+else if (thirdSide == secondSide + firstSide)
+{
+    Console.WriteLine("Valid Triangle");
+}
+else
+{
+    Console.WriteLine("Invalid Triangle");
+}
